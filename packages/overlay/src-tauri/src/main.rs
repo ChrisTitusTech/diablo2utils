ad9599c7c9cc6cbb92306726c24ab11c0525b704
titus <contact@christitus.com>
2026-03-20 19:03:54 -0500

    switch overlay to electron

--- packages/overlay/src-tauri/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    diablo2_overlay_lib::run();
-}
--- packages/overlay/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { OverlayApp } from './overlay.js';
-export { MinimapRenderer } from './minimap.renderer.js';
-export { OverlayWsClient } from './ws.client.js';
